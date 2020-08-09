b4f95074a12e6427c2401a80fa621bcdb4f5d4ca
Tracy Wong <neshamah00000@gmail.com>
2020-08-09 17:51:47 +0800

    Updates

---
+++ precache-manifest.d4b4a86a4434813d36b903e462e28fdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1765b7562c488265106712bfa2742d",
+    "url": "/typing/index.html"
+  },
+  {
+    "revision": "b359316a0a4ca70d622e",
+    "url": "/typing/static/css/main.5cfcac65.chunk.css"
+  },
+  {
+    "revision": "8f920edb6f1431fd84d1",
+    "url": "/typing/static/js/2.c24ca343.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typing/static/js/2.c24ca343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b359316a0a4ca70d622e",
+    "url": "/typing/static/js/main.0a02c2d8.chunk.js"
+  },
+  {
+    "revision": "c5f64a8d2403e5f89708",
+    "url": "/typing/static/js/runtime-main.175ab308.js"
+  }
+]);
